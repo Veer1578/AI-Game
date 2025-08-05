d8b38a090fb4ed6361fb902d2a19cdddd105b9f7
Veer1578 <sweetyatulshah@gmail.com>
2025-08-05 16:36:58 +0530

    AI games

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -6,6 +6,8 @@ init(autoreset=True)
 def rock_paper_scissor():
     print(f"{Fore.CYAN}Welcome to Rock Paper Scissors!")
     game = True
+    comp_score = 0
+    player_score = 0
 
     while game:
         print(f"{Fore.CYAN}Choose {Fore.LIGHTBLACK_EX}Rock{Fore.CYAN}, {Fore.WHITE}Paper{Fore.CYAN}, {Fore.RED}Scissors")
@@ -19,20 +21,39 @@ def rock_paper_scissor():
                 print(f"{Fore.YELLOW}It's a tie!")
             elif computer_choice == 'paper':
                 print(f"{Fore.RED}Computer wins!")
+                comp_score += 1
+                print(f"{Fore.CYAN}Computer score: {comp_score}")
+                print(f"{Fore.CYAN}Player score: {player_score}")
             else:
                 print(f"{Fore.GREEN}You win!")
+                player_score += 1
+                print(f"{Fore.CYAN}Player score: {player_score}")
+                print(f"{Fore.CYAN}Computer score: {comp_score}")
         elif player_choice == 'paper':
             if computer_choice == 'rock':
                 print(f"{Fore.GREEN}You win!")
+                player_score += 1
+                print(f"{Fore.CYAN}Player score: {player_score}")
+                print(f"{Fore.CYAN}Computer score: {comp_score}")
+                
             elif computer_choice == 'paper':
                 print(f"{Fore.YELLOW}It's a tie!")
             else:
                 print(f"{Fore.RED}Computer wins!")
+                comp_score += 1
+                print(f"{Fore.CYAN}Computer score: {comp_score}")
+                print(f"{Fore.CYAN}Player score: {player_score}")
         elif player_choice == 'scissors':
             if computer_choice == 'rock':
                 print(f"{Fore.RED}Computer wins!")
+                comp_score += 1
+                print(f"{Fore.CYAN}Computer score: {comp_score}")
+                print(f"{Fore.CYAN}Player score: {player_score}")
             elif computer_choice == 'paper':
                 print(f"{Fore.GREEN}You win!")
+                player_score += 1
+                print(f"{Fore.CYAN}Player score: {player_score}")
+                print(f"{Fore.CYAN}Computer score: {comp_score}")
             else:
                 print(f"{Fore.YELLOW}It's a tie!")
         else:
@@ -42,6 +63,8 @@ def rock_paper_scissor():
         if play_again == 'y':
             continue
         else:
+            print(f"{Fore.CYAN}Final Player score: {player_score}")
+            print(f"{Fore.CYAN}Final Computer score: {comp_score}")
             break
 
 if __name__ == "__main__":
